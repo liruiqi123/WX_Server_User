bf56680e1bca724901b1ce25674e7965a0673302
liruiqi123 <1595854626@qq.com>
2018-09-26 17:56:45 +0800

    添加ajaxReturn方法

--- application/user/controller/Login.php
+++ application/user/controller/Login.php
@@ -13,6 +13,46 @@ class Login
     }
 
 
+     /**
+         * Ajax方式返回数据到客户端
+         * @access protected
+         * @param mixed $data 要返回的数据
+         * @param String $type AJAX返回数据格式
+         * @param int $json_option 传递给json_encode的option参数
+         * @return void
+         */
+        protected function ajaxReturn($data,$type='',$json_option=0) {
+            if(empty($type)) $type  =   C('DEFAULT_AJAX_RETURN');
+            switch (strtoupper($type)){
+                case 'JSON' :
+                    // 返回JSON数据格式到客户端 包含状态信息
+                    header('Content-Type:application/json; charset=utf-8');
+                    exit(json_encode($data,$json_option));
+                case 'XML'  :
+                    // 返回xml格式数据
+                    header('Content-Type:text/xml; charset=utf-8');
+                    exit(xml_encode($data));
+                case 'JSONP':
+                    // 返回JSON数据格式到客户端 包含状态信息
+                    header('Content-Type:application/json; charset=utf-8');
+                    $handler  =   isset($_GET[C('VAR_JSONP_HANDLER')]) ? $_GET[C('VAR_JSONP_HANDLER')] : C('DEFAULT_JSONP_HANDLER');
+                    exit($handler.'('.json_encode($data,$json_option).');');
+                case 'EVAL' :
+                    // 返回可执行的js脚本
+                    header('Content-Type:text/html; charset=utf-8');
+                    exit($data);
+                default     :
+                    // 用于扩展其他返回格式数据
+                    Hook::listen('ajax_return',$data);
+            }
+        }
+
+
+
+
+
+
+
     public function postUserLogin($code,$gameKey)
     {
         $url = "https://api.weixin.qq.com/sns/jscode2session?appid=wx7a1aa038211464b4&secret=56a72e174923c1bdadd7cb39fefa101f&js_code=$code&grant_type=authorization_code";
