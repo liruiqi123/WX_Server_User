11216c7dfbd06416ee46642992e86fcd441afa9e
liruiqi123 <1595854626@qq.com>
2018-09-27 13:40:34 +0800

    修改登录方法，将openid作为token进行返回

--- application/user/controller/Login.php
+++ application/user/controller/Login.php
@@ -44,6 +44,24 @@ class Login extends Controller
 
 
 
+    public function getUserOpenid($code)
+    {
+        $url = "https://api.weixin.qq.com/sns/jscode2session?appid=wx7a1aa038211464b4&secret=56a72e174923c1bdadd7cb39fefa101f&js_code=$code&grant_type=authorization_code";
+
+        $info = file_get_contents($url);
+
+        $json = json_decode($info);//对json数据解码
+
+        $arr = get_object_vars($json);
+
+        $openid = $arr['openid'];
+
+        $session_key = $arr['session_key'];
+
+        return $openid;
+
+     }
+
 
 
 
@@ -63,11 +81,10 @@ class Login extends Controller
         $openid = $arr['openid'];
  
         $session_key = $arr['session_key'];
-        
-        
+
         $result = Db::execute('insert into user_login (gamekey,code,openid,session_key) values (?,?,?,?)',[$gameKey,$code,$openid,$session_key]);
-        dump($result);
-        return $result;
+
+        return $openid;
 
      }
 
@@ -77,9 +94,11 @@ class Login extends Controller
 
         $result = Db::execute('insert into user_info (gamekey,code,picture,city,country,gender,language,nickName,province) values (?,?,?,?,?,?,?,?,?)',[$gameKey,$code,$picture,$city,$country,$gender,$language,$nickName,$province]);
 
+        $token =  $this->postUserLogin($code,$gameKey);
+
         if ($result == 1) {
 
-           $this->ajaxReturn(['code'=>20000, 'msg'=>'操作成功']);
+           $this->ajaxReturn(['code'=>20000, 'msg'=>'操作成功','token'=>$token]);
 
         }else{
 
