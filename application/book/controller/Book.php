39ca6daba57846ae086c5be3fb2e06b2c1cb7840
liruiqi123 <1595854626@qq.com>
2018-10-15 17:52:42 +0800

    更改return的格式 https://www.kancloud.cn/manual/thinkphp5/118017

--- application/book/controller/Book.php
+++ application/book/controller/Book.php
@@ -18,7 +18,7 @@ class Book
 
         $data = $bookQuestionField->select();
 
-        return ['data'=>$data,'code'=>1,'message'=>'操作完成'];
+        $this->return ['data'=>$data,'code'=>1,'message'=>'操作完成'];
     }
 
 
